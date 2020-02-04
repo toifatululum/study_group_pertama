94557dfcab01f7aeb2b7b8de2e2be3c003857438
toifatululum <toifatululum222@gmail.com>
2020-02-04 21:05:56 +0700

    sg_2

--- app.js
+++ app.js
@@ -2,63 +2,114 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 
-//set parsing
-app.use(express.json()); //json
-app.use(express.urlencoded({ extended: true })); //parsing url
+// set parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-//data buatan
+// data buatan
 let courses = [
   {
-    id: 1001,
-    title: "react js",
-    description: "asdasdasd"
+    id: 1,
+    title: "Spiderman ",
+    description: "aassdasd",
+    price: 50000
   },
   {
-    id: 1002,
-    title: "react js v2",
-    description: "qweet"
+    id: 2,
+    title: "Aquaman",
+    description: "asdasdasd",
+    price: 30000
   },
   {
-    id: 1003,
-    title: "react js v3",
-    description: "asdasdasdmmmm"
+    id: 3,
+    title: "Disney Mulan",
+    description: "asdasdasd",
+    price: 30000
   }
 ];
 
-//menampilkan data
-app.get("/api/courses", (req, res, next) => {
-  res.status(200).json(courses); //200,400 in client, 500 in server
+app.get("/", (req, res, next) => {
+  res.status(200).send("Hi from me!");
 });
 
-//show one data
-app.get("/api/courses/:idCourse", (req, res, next) => {
-  //   courses.find(data => {
-  //     if (data.id == req.params.idCourse) {
-  //       return res.status(200).json(data);
+// show all data
+app.get("/api/course", (req, res, next) => {
+  res.status(200).json(courses);
+});
+
+// show single data
+app.get("/api/course/:id", (req, res, next) => {
+  // courses.find(data => {
+  //     if (data.id == req.params.id) {
+  //         return res.status(200).json(data);
   //     }
-  //   });
-  var dataFix = courses.find(data => data.id == req.params.idCourse);
+  // });
+
+  var dataFix = courses.find(data => data.id == req.params.id);
+
   if (!dataFix) {
     return res
       .status(404)
-      .json({ message: `id ${req.params.id} Tidak ditemukan` });
+      .json({ message: `ID ${req.params.id} Tidak ditemukan!` });
   }
-  return res.status(200).json(dataFix);
+
+  res.status(200).json(dataFix);
 });
 
-//insert data
-app.post("/api/courses", (req, res, next) => {
-  const title = req.body.title;
-  const description = req.body.description;
+// insert data
+app.post("/api/course", (req, res, next) => {
+  // pemborosan
+  // const titleCourse = req.body.title;
+  // const description = req.body.description;
+  // const price = req.body.price;
+
+  const { title, description, price } = req.body;
 
   var data = {
     id: courses.length + 1,
     title,
-    description
+    description,
+    price
   };
 
   courses.push(data);
   res.status(200).json(courses);
 });
 
-app.listen(PORT, () => console.log(`server runing on ${PORT}`));
+// //delete data
+//map itu sama aja kaya filter
+//param (api/:id)
+//query pake ?key , value dipake saat mengambil data dari url
+app.delete("/api/course/:id", (req, res, next) => {
+  const id = req.params.id;
+  var validasi = courses.find(data => data.id == id);
+  if (!validasi) {
+    return res.status(200).json({
+      error: true,
+      message: `ID ${id} tidk ditemukan!!`
+    });
+  }
+  var data = courses.filter(data => data.id != id);
+  //app update
+  courses = data;
+  res.status(200).json(courses);
+});
+
+// kalau ada res.status di tengah, pakai return biar ngga error
+app.put("/api/course/:id", (req, res, next) => {
+  const id = req.params.id;
+  var course = courses.find(data => data.id == id);
+  if (!course) {
+    return res
+      .status(404)
+      .json({ error: true, message: `ID ${req.params.id} tidak ditemukan` });
+  }
+  const { title, description, price } = req.body;
+  course.title = title;
+  course.description = description;
+  courses.price = price;
+
+  res.status(200).json(courses);
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
